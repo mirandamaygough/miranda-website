a14651321d1ce4e7b3ae7e0bc7cf91439ade6aed
Miranda Gough <mirandamaygough@gmail.com>
2025-10-22 20:47:37 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,88 @@
+import { Domine } from "next/font/google";
 import Image from "next/image";
+
+const domine = Domine({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-domine",
+});
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <h1 className={`${domine.className} text-6xl text-center sm:text-left`}>
+          Miranda Gough
+        </h1>
+        <h2 className="text-3xl text-center sm:text-left">
+          tl;dr:
+        </h2>
+        <ul className="list-disc list-inside text-2xl">
+          <li className="text-2xl mt-2">Final year BSc Computer Science undergraduate @ University of Leeds</li>
+          <li className="text-2xl">Data science and software engineering internships @ PwC</li>
+          <li className="text-2xl">President @ Leeds Computing Society and co-founder of LeedsHack</li>
+          <li className="text-2xl">Looking for opportunities to work as a software engineer, data scientist or technical analyst</li>
+        </ul>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <h2 className="text-3xl text-center sm:text-left">
+          Experience
+        </h2>
+          <h3 className="text-2xl flex justify-between items-center">
+            <div className="mr-4 bg-white rounded-lg p-2 flex items-center justify-center">
+              <Image
+                src="/pwc-logo.png"
+                alt="PwC Logo"
+                width={80}
+                height={60}
+              />
+            </div>
+            <span>Intern - Data Scientist in the AI Research team @ PwC UK (Jun 2024 - Aug 2025)</span>
+          </h3>
+          <h3 className="text-2xl flex justify-between items-center">
+            <div className="mr-4 bg-white rounded-lg p-2 flex items-center justify-center">
+              <Image
+                src="/pwc-logo.png"
+                alt="PwC Logo"
+                width={80}
+                height={60}
+              />
+            </div>
+            <span>Intern - Software Engineer in the Data & Automation team @ PwC UK (Jun 2023 - Aug 2023)</span>
+          </h3>
+          <h3 className="text-2xl flex justify-between items-center">
+            <div className="mr-4 bg-white rounded-lg flex items-center justify-center">
+              <Image
+                src="/compsoc-logo.svg"
+                alt="Leeds Computing Society Logo"
+                width={60}
+                height={60}
+              />
+            </div>
+            <span>President @ Leeds Computing Society (Jun 2025 - Present)</span>
+          </h3>
+          <h3 className="text-2xl flex justify-between items-center">
+            <div className="mr-4 bg-white rounded-lg flex items-center justify-center">
+              <Image
+                src="/compsoc-logo.svg"
+                alt="Leeds Computing Society Logo"
+                width={60}
+                height={60}
+              />
+            </div>
+            <span>Equality, Diversity and Inclusion Secretary @ Leeds Computing Society (Jun 2023 - Jun 2025)</span>
+          </h3>
+          <h3 className="text-2xl flex justify-between items-center">
+            <div className="mr-4 bg-white rounded-lg flex items-center justify-center">
+              <Image
+                src="/leedshack-logo.png"
+                alt="LeedsHack Logo"
+                width={80}
+                height={60}
+              />
+            </div>
+            <span>Co-founder and Experience Lead @ LeedsHack (Jun 2023 - Jun 2025)</span>
+          </h3>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
